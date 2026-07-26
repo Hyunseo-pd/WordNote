0e3546b357067550fb22bea8c2b767164e031da7
unknown <hseo100@dgist.ac.kr>
2026-07-26 11:34:21 +0900

    getdocs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+import {
+  doc,
+  setDoc,
+  deleteDoc,
+  collection,
+  getDocs,
+} from "firebase/firestore";
+
 import GermanWordForm from "./GermanWordForm.jsx";
 import JapaneseWordForm from "./JapaneseWordForm.jsx";
 
